0282af76de6da49d8ada8219ee82ca9aabe37110
shimizu <noon3m.op.sh@gmail.com>
2025-04-24 23:38:14 +0900

    [add] 作品テーブル

---
+++ back/docker/mysql/work.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS works (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  event_id VARCHAR(255),
+  profile_id VARCHAR(255),
+  title VARCHAR(255),
+  description TEXT,
+  FOREIGN KEY (event_id) REFERENCES events(id),
+  FOREIGN KEY (profile_id) REFERENCES profiles(id),
+);
