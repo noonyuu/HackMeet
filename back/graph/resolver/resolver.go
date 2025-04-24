bb77cf21e93c4a4bdc36363a620f480a08abd8b5
shimizu <noon3m.op.sh@gmail.com>
2025-04-24 22:52:50 +0900

    [add] graphQLをうごかすところまで

---
+++ back/graph/resolver/resolver.go
@@ -0,0 +1,20 @@
+package resolver
+
+//go:generate go run github.com/99designs/gqlgen generate
+import (
+	"database/sql"
+
+	"github.com/noonyuu/nfc/back/graph/model"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	DB       *sql.DB
+	user     *model.User
+	users    []*model.User
+	profile  *model.Profile
+	profiles []*model.Profile
+}
---
+++ back/internal/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/noonyuu/nfc/back/graph"
+	"github.com/noonyuu/nfc/back/graph/resolver"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+func NewRouter(db *sql.DB) http.Handler {
+	mux := http.NewServeMux()
+
+	// Hello エンドポイント
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!!!!!!"))
+	})
+
+	// GraphQL handler 設定
+	srv := handler.New(graph.NewExecutableSchema(graph.Config{
+		Resolvers: &resolver.Resolver{
+			DB: db,
+		},
+	}))
+
+	srv.AddTransport(transport.Options{})
+
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
+
+	mux.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	mux.Handle("/query", srv)
+
+	return mux
+}
