57c1f2feb085047ed09c4801715e286fb12e19ab
shimizu <noon3m.op.sh@gmail.com>
2025-04-24 22:58:17 +0900

    [change] テーブル作成時にIF NOT EXISTSを追加

--- back/docker/mysql/profile.sql
+++ back/docker/mysql/profile.sql
@@ -1,8 +1,10 @@
-CREATE TABLE profiles (
+CREATE TABLE IF NOT EXISTS profiles (
   id VARCHAR(255) PRIMARY KEY,
   user_id VARCHAR(255),
   avatar_url VARCHAR(255),
   nick_name VARCHAR(255),
+  graduation_year INT,
+  affiliation VARCHAR(255),
   bio TEXT,
   created_at DATETIME,
   updated_at DATETIME,
