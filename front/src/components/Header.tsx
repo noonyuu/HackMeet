8665d60496224297b320ca9d52a9fddd9b9292dd
shimizu <noon3m.op.sh@gmail.com>
2025-05-07 02:10:27 +0900

    [change] 認証系画面の時のみヘッダーの中身を変えるようにした

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,28 +1,43 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useMatch } from "@tanstack/react-router";
 
 export const Header = () => {
+  // ログイン or 新規登録時かを判別
+  const isRegister = useMatch({ from: "/register", shouldThrow: false });
+  const isLogin = useMatch({ from: "/login", shouldThrow: false });
+
+  const isAuth = isRegister || isLogin;
+
   return (
-    <header className="flex h-25 border-b border-gray-300 px-8 pt-4">
-      <div className="flex flex-col w-full">
-        <div className="flex flex-row items-center">
-          <div className="grow text-2xl flex">タイトル</div>
-          <div className="">icon</div> {/* ログイン ? icon : ログイン or 新規登録 */}
-        </div>
-        <div className="flex grow items-end gap-4">
-          <Link
-            to="/"
-            className="text-lg [&.active]:border-b [&.active]:border-green-500 [&.active]:font-bold"
-          >
-            作品一覧
-          </Link>
-          <Link
-            to="/about"
-            className="text-lg [&.active]:border-b [&.active]:border-green-500 [&.active]:font-bold"
-          >
-            イベント
-          </Link>
-        </div>
-      </div>
-    </header>
+    <>
+      {isAuth ? (
+        <header className="flex h-14 items-center justify-center border-b border-gray-300">
+          <div className="font-main text-2xl">タイトル</div>
+        </header>
+      ) : (
+        <header className="flex h-25 justify-center border-b border-gray-300 px-8 pt-4">
+          <div className="font-main flex w-full flex-col">
+            <div className="flex flex-row items-center">
+              <div className="flex grow text-2xl">タイトル</div>
+              <div className="">icon</div>{" "}
+              {/* ログイン ? icon : ログイン or 新規登録 */}
+            </div>
+            <div className="flex grow items-end gap-4">
+              <Link
+                to="/"
+                className="text-lg [&.active]:border-b [&.active]:border-green-500 [&.active]:font-bold"
+              >
+                作品一覧
+              </Link>
+              <Link
+                to="/about"
+                className="text-lg [&.active]:border-b [&.active]:border-green-500 [&.active]:font-bold"
+              >
+                イベント
+              </Link>
+            </div>
+          </div>
+        </header>
+      )}
+    </>
   );
 };
