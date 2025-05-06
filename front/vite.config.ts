fcc4e09630087bdd6bfa33e2ca03f601d4d4486e
shimizu <noon3m.op.sh@gmail.com>
2025-05-06 23:23:27 +0900

    [fix] tsconfig.app.jsonのパスを修正し、vite.config.tsでのエイリアス設定を更新

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
