53c2e19ffeff3efecb07e747be05e6a720017a78
shimizu <noon3m.op.sh@gmail.com>
2025-05-13 15:14:00 +0900

    [change] redisとgraphqlを使えるように初期化

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/noonyuu/nfc/back/graph/resolver"
 	"github.com/noonyuu/nfc/back/internal/config"
 	"github.com/noonyuu/nfc/back/internal/infrastructure/db"
 	"github.com/noonyuu/nfc/back/internal/server"
@@ -14,16 +15,25 @@ func main() {
 	fmt.Println("Starting the application...")
 
 	cfg := config.Load()
-	dbConn, err := db.Connect(cfg)
+
+	// MySQLの初期化
+	dbConn, err := db.ConnectMysql(cfg)
 	if err != nil {
 		log.Fatal("DB接続エラー: ", err)
 	}
 	defer dbConn.Close()
 
-	fmt.Println("DB接続成功")
+	// Redisの初期化
+	redisConn, err := db.ConnectRedis()
+	if err != nil {
+		log.Fatal("Redis接続エラー: ", err)
+	}
+	defer redisConn.Close()
 
+	// GraphQLの初期化
+	graphql := &resolver.Resolver{DB: dbConn}
 	// サーバー起動
-	handler := server.NewRouter(dbConn)
+	handler := server.NewRouter(dbConn, redisConn, graphql)
 	err = http.ListenAndServe(":8080", handler)
 	if err != nil {
 		log.Fatal("サーバー起動エラー: ", err)
