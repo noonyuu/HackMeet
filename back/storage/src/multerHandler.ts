f259f065235df2a1026a5975ff460e846aa22927
shimizu <noon3m.op.sh@gmail.com>
2025-05-15 14:47:18 +0900

    [change] デバッグように入れていた部分をコメント

--- back/storage/src/multerHandler.ts
+++ back/storage/src/multerHandler.ts
@@ -1,21 +1,21 @@
 import multer from "multer";
-import fs from "fs";
-import path from "path";
+// import fs from "fs";
+// import path from "path";
 
 // ファイルの保存先とファイル名を指定
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // 画像がuploadされるパス
-    const uploadPath = path.resolve(__dirname, "../uploads");
+  // destination: (req, file, cb) => {
+  //   // 画像がuploadされるパス
+  //   const uploadPath = path.resolve(__dirname, "./uploads");
 
-    // uploadPathにディレクトリが存在するかどうかを確認
-    if (!fs.existsSync(uploadPath)) {
-      // uploadPathにディレクトリが存在しない場合、ディレクトリを作成
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
+  //   // uploadPathにディレクトリが存在するかどうかを確認
+  //   if (!fs.existsSync(uploadPath)) {
+  //     // uploadPathにディレクトリが存在しない場合、ディレクトリを作成
+  //     fs.mkdirSync(uploadPath, { recursive: true });
+  //   }
 
-    cb(null, uploadPath);
-  },
+  //   cb(null, uploadPath);
+  // },
   // アップロードされるファイル名を作成
   filename: (req, file, cb) => {
     const uniqueSuffix = Math.random().toString(26).substring(4, 10);
