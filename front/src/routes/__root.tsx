394050281a4e88b7d70f4272693f25e52d7ed5cf
shimizu <noon3m.op.sh@gmail.com>
2025-05-18 17:59:51 +0900

    [add] 認証コンテキストを使用したルートコンポーネントの実装

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -1,15 +1,23 @@
 import { Header } from "@/components/Header";
+import { AuthContext } from "@/contexts/authContext";
+import { useAuth } from "@/hooks/useAuth";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-export const Route = createRootRoute({
-  component: () => (
-    <>
+function RootComponent() {
+  const { user, isAuthenticated, login, logout } = useAuth();
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       <Header />
-      <main className="flex w-screen flex-col items-center justify-center">
+      <main className="flex w-full flex-col items-center justify-center">
         <Outlet />
       </main>
       <TanStackRouterDevtools />
-    </>
-  ),
+    </AuthContext.Provider>
+  );
+}
+
+export const Route = createRootRoute({
+  component: RootComponent,
 });
