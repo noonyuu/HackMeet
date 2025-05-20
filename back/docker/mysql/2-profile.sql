c392575bcb8a57a300049dc910332181069d45c2
shimizu <noon3m.op.sh@gmail.com>
2025-05-20 12:28:53 +0900

    [fix] シンプルにミスを修正

--- back/docker/mysql/2-profile.sql
+++ back/docker/mysql/2-profile.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS providers (
+CREATE TABLE IF NOT EXISTS profiles (
   id VARCHAR(255) PRIMARY KEY,
-  user_id VARCHAR(255),
-  provider_id VARCHAR(255),
-  provider VARCHAR(255),
+  avatar_url VARCHAR(255),
+  nick_name VARCHAR(255),
+  graduation_year INT,
+  affiliation VARCHAR(255),
+  bio TEXT,
   created_at DATETIME,
-  updated_at DATETIME,
-  UNIQUE KEY (user_id, provider_id),
-  CONSTRAINT fk_providers_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  updated_at DATETIME
 ) ENGINE=InnoDB;
