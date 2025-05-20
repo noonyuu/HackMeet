c1b76f866022ec79650ba415a9d9a599a6013f35
shimizu <noon3m.op.sh@gmail.com>
2025-05-20 15:40:33 +0900

    [fix] コピペのままやった

--- back/docker/mysql/4-work_event.sql
+++ back/docker/mysql/4-work_event.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS work_profiles (
+CREATE TABLE IF NOT EXISTS work_events (
   id INT AUTO_INCREMENT PRIMARY KEY,
   work_id VARCHAR(255),
-  profile_id VARCHAR(255),
+  event_id VARCHAR(255),
   created_at DATETIME,
   updated_at DATETIME,
   FOREIGN KEY (work_id) REFERENCES works(id),
-  FOREIGN KEY (profile_id) REFERENCES profiles(id)
+  FOREIGN KEY (event_id) REFERENCES profiles(id)
 ) ENGINE=InnoDB;
