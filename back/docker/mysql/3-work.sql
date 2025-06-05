e9a404d3c48281b939bfceca2bfe966b85b85d4f
shimizu <noon3m.op.sh@gmail.com>
2025-06-06 03:53:00 +0900

    [add] 画像およびダイアグラム画像のテーブルを追加し、ワーク関連の画像テーブルを作成

--- back/docker/mysql/3-work.sql
+++ back/docker/mysql/3-work.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS works (
   id VARCHAR(255) PRIMARY KEY,
   title VARCHAR(255),
   description TEXT,
-  image_url TEXT,
   created_at DATETIME,
   updated_at DATETIME
 ) ENGINE=InnoDB;
