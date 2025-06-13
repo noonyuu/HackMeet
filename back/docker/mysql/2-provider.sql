52f54a11c354e75063b3ef4b5ce0b30e15f7bc10
shimizu <noon3m.op.sh@gmail.com>
2025-06-14 00:09:24 +0900

    [fix] providersテーブルのuser_idカラムのユニーク制約を削除

--- back/docker/mysql/2-provider.sql
+++ back/docker/mysql/2-provider.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS providers (
   id VARCHAR(255) PRIMARY KEY,
-  user_id VARCHAR(255) UNIQUE,
+  user_id VARCHAR(255),
   provider_id VARCHAR(255) UNIQUE,
   provider VARCHAR(255),
   created_at DATETIME,
